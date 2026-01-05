97abd5311979aa6840a2eea8b274b60b99c0bd57
Scott Schlesier <scott@schlesier.ca>
2026-01-05 13:46:34 +0900

    switch to sax xml parser

--- extract-pdfs.js
+++ extract-pdfs.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const XmlStream = require("xml-stream");
+const sax = require("sax");
 
 //run this on many enex files using a command like
 //parallel node extract-pdfs.js {} ::: $DESKTOP/export/done/*.enex
@@ -13,46 +13,56 @@ fs.mkdirSync(notebookPath, { recursive: true });
 console.log(notebookPath);
 
 const textStream = fs.createReadStream(pathToEnex);
-const xmlStream = new XmlStream(textStream);
+const parser = sax.createStream(true, { trim: true });
 
 var note = {};
+var currentElement = null;
+var elementStack = [];
+var currentText = "";
 
-xmlStream.on("startElement: note", (el) => {
-  note = {};
-});
-
-xmlStream.on("endElement: title", (el) => {
-  // console.log(JSON.stringify(el, null, 4));
-  note.title = el.$text;
-});
-
-xmlStream.on("endElement: data", (el) => {
-  // console.log(JSON.stringify(el, null, 4));
-  note.data = el.$text;
-});
-
-xmlStream.on("endElement: file-name", (el) => {
-  // console.log(JSON.stringify(el, null, 4));
-  if (el.$text) {
-    note.fileName = el.$text;
-  } else {
-    note.fileName = note.title + ".pdf";
+parser.on("opentag", (node) => {
+  elementStack.push(node.name);
+  currentElement = node.name;
+  currentText = "";
+  
+  if (node.name === "note") {
+    note = {};
   }
 });
 
-xmlStream.on("endElement: mime", (el) => {
-  // console.log(JSON.stringify(el, null, 4));
-  note.mime = el.$text;
+parser.on("text", (text) => {
+  currentText += text;
 });
 
-xmlStream.on("endElement: note", (el) => {
-  if (note.mime === "application/pdf") {
-    const outputPath = makeOutputPath();
-    console.log("dumping " + outputPath);
-    const buffer = Buffer.from(note.data, "base64");
-    fs.writeFile(outputPath, buffer, function (err) {});
+parser.on("closetag", (tagName) => {
+  if (tagName === "note") {
+    if (note.mime === "application/pdf") {
+      const outputPath = makeOutputPath();
+      console.log("dumping " + outputPath);
+      const buffer = Buffer.from(note.data, "base64");
+      fs.writeFile(outputPath, buffer, function (err) {});
+    }
+    note = {};
+  } else if (tagName === "title") {
+    note.title = currentText;
+  } else if (tagName === "data") {
+    note.data = currentText;
+  } else if (tagName === "file-name") {
+    if (currentText) {
+      note.fileName = currentText;
+    } else {
+      note.fileName = note.title + ".pdf";
+    }
+  } else if (tagName === "mime") {
+    note.mime = currentText;
   }
+  
+  elementStack.pop();
+  currentElement = elementStack.length > 0 ? elementStack[elementStack.length - 1] : null;
+  currentText = "";
 });
+
+textStream.pipe(parser);
 
 function makeOutputPath() {
   var ext = path.extname(note.fileName);
